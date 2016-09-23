2f659a3430dd7b451b51f78a005586b8be0d23a1
nealio82 <nealio82@gmail.com>
2016-09-23 11:08:59 +0100

    Update authentication URI
    Authentication API URI has changed and need to append v2.0 to the URL

--- src/MediaMath/TerminalOneAPI/Infrastructure/ApiHost.php
+++ src/MediaMath/TerminalOneAPI/Infrastructure/ApiHost.php
@@ -8,7 +8,7 @@ namespace MediaMath\TerminalOneAPI\Infrastructure;
  */
 final class ApiHost
 {
-    const T1_AUTHENTICATION = 'https://api.mediamath.com/api/';
+    const T1_AUTHENTICATION = 'https://api.mediamath.com/api/v2.0/';
     const T1_MANAGEMENT = 'https://api.mediamath.com/api/v2.0/';
     const T1_VIDEO = 'https://api.mediamath.com/video/v1/';
     const T1_REPORTING = 'https://api.mediamath.com/reporting/v1/std/';
