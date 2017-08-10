1a7f7cffba5a79979bcecc9a8535798c1da1f6ac
gabe <en1roo@gmail.com>
2017-08-10 14:30:18 +0100

    performance_social endpiont introduced

---
+++ src/MediaMath/TerminalOneAPI/Reporting/PerformanceSocial.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace MediaMath\TerminalOneAPI\Reporting;
+
+use MediaMath\TerminalOneAPI\Infrastructure\Endpoint;
+use MediaMath\TerminalOneAPI\Infrastructure\ReportingApiObject;
+use MediaMath\TerminalOneAPI\Infrastructure\NonCreateable;
+use MediaMath\TerminalOneAPI\Infrastructure\NonDeletable;
+use MediaMath\TerminalOneAPI\Infrastructure\NonUpdateable;
+
+/**
+ * Class Performance
+ * @package MediaMath\TerminalOneAPI\Reporting
+ */
+class PerformanceSocial extends ReportingApiObject implements Endpoint
+{
+
+    use NonDeletable;
+    use NonUpdateable;
+    use NonCreateable;
+
+    /**
+     * @return string
+     */
+    public function endpoint()
+    {
+        return 'performance_social';
+    }
+
+}
